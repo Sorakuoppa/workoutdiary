69b8837a6962a27222654bc5bae56afa1a7623f7
Sorakuoppa <n2hyti00@students.oamk.fi>
2024-03-04 13:31:36 +0200

    home screen poistettu tarpeettomana

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import Home from "./components/Home";
 import Settings from "./components/Settings";
 import List from "./components/ListView";
 import AddExercise from "./components/AddExercise";
@@ -12,11 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name={"Home"} component={Home} icon={"home"}
-        options={{tabBarIcon: () => <Ionicons name={"home"} size={24} color={"black"} />}}/>
         <Tab.Screen name={"Add Exercise"} component={AddExercise} 
         options={{tabBarIcon: () => <Ionicons name={"add-circle-outline"} size={24} color={"black"} />}}/>
-        <Tab.Screen name={"List"} component={List} 
+        <Tab.Screen name={"Exercise list"} component={List} 
         options={{tabBarIcon: () => <Ionicons name={"list-outline"} size={24} color={"black"} />}}/>
         <Tab.Screen name={"Settings"} component={Settings} 
         options={{tabBarIcon: () => <Ionicons name={"settings-outline"} size={24} color={"black"} />}}/>
